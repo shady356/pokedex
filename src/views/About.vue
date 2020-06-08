dbbcca7176b117aef825546b9782d55a37fc3cf3
shady356 <heenriko@gmail.com>
2020-06-09 01:45:35 +0200

    header on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,31 +1,49 @@
 <template>
-  <div class="about-container">
-    <h1>About Pokedex</h1>
-    <p>
-      A small application to present Pokemon and their in-game meta data The
-      data is served from PokéAPI (https://pokeapi.co/), a RESTful Pokemon API.
-    </p>
-    <h2>Project</h2>
-    <p>
-      The project is available from GitHub (https://github.com/shady356/pokedex)
-    </p>
-    <h2>Resources</h2>
-    <p>
-      Pokemon type icons from:
-      https://github.com/duiker101/pokemon-type-svg-icons. UI icons from Font
-      Awesome
-    </p>
+  <div>
+    <Header>
+      <template #title>
+        <h3>About PokéView</h3>
+      </template> 
+    </Header>
+    <div class="about-container">
+      <p>
+        A small application to present Pokemon and their in-game meta data. The
+        data is served from PokéAPI (https://pokeapi.co/), a RESTful Pokemon API.
+      </p>
+      <h4>Project</h4>
+      <p>
+        The project is available from GitHub (https://github.com/shady356/pokedex)
+      </p>
+      <h4>Resources</h4>
+      <p>
+        Pokemon type icons from:
+        https://github.com/duiker101/pokemon-type-svg-icons. UI icons from Font
+        Awesome
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from "@/components/layout/Header.vue";
 export default {
-  name: "About"
+  name: "About",
+  components: {
+    Header
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .about-container {
   margin: $l;
+
+  h4 {
+    margin: $xs 0 $s;
+  }
+
+  p {
+    line-height: 1.3rem;
+  }
 }
 </style>
